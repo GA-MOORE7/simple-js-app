9f4fdf2d7adfccf233eda24b7bcca59bf0abe71e
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-05-16 00:55:23 +0200

    editing array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [
-    {name: 'Pikachu', height: 0.4, types: ['electric']},
-    {name: 'Pidgeot', height: 1.5, types: ['normal', 'flying']},
-    {name: 'Horsea', height: 0.4, types: ['water']},
-    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-    {name: 'Beedrill', height: 1, types: ['bug', 'poison']}
+    {name: 'Pikachu', height: 0.4, type: 'electric'},
+    {name: 'Pidgeot', height: 1.5, type: ['normal', 'flying']},
+    {name: 'Horsea', height: 0.4, type: 'water'},
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+    {name: 'Beedrill', height: 1, type: ['bug', 'poison']}
 ];
 
