3efe9f2f880e4fa7e84b264a0497182ecf4ce945
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-05-16 00:48:31 +0200

    create an array with Pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Pikachu', height: 0.4, types: ['electric']},
+    {name: 'Pidgeot', height: 1.5, types: ['normal', 'flying']},
+    {name: 'Horsea', height: 0.4, types: ['water']},
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+    {name: 'Beedrill', height: 1, types: ['bug', 'poison']}
+];
+
