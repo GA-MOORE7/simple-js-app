844c9b51ea3541f6792707fa9d93c4927943cfcd
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-05-24 01:12:54 +0200

    Change for loop into forEach() function

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,11 @@ let pokemonList = [
     {name: 'Beedrill', height: 1, type: ['bug', 'poison']}
 ];
 
-// Create a list of Pokemon using a for loop (+ conditional)
-for (let i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 1.6) {
-        document.write('<li>' + pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + '</li>');
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height < 1.6) {
+        document.write('<li>' + pokemon.name + ' ' + '(height: ' + pokemon.height + ')' + '</li>');
     } else {
-        document.write('<li>' + pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</li>'); 
+        document.write('<li>' + pokemon.name + ' ' + '(height: ' + pokemon.height + ')' + ' - Wow, that\'s big!' + '</li>'); 
     }   
 }
-
+);
