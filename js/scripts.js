24ce8fb8ea991e55e50a29fe27b97a33e775d54f
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-05-24 01:23:46 +0200

    Wrap Pokemon list array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-let pokemonList = [
-    {name: 'Pikachu', height: 0.4, type: 'electric'},
-    {name: 'Pidgeot', height: 1.5, type: ['normal', 'flying']},
-    {name: 'Horsea', height: 0.4, type: 'water'},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Beedrill', height: 1, type: ['bug', 'poison']}
-];
+(function () {
+    let pokemonList = [
+        {name: 'Pikachu', height: 0.4, type: 'electric'},
+        {name: 'Pidgeot', height: 1.5, type: ['normal', 'flying']},
+        {name: 'Horsea', height: 0.4, type: 'water'},
+        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+        {name: 'Beedrill', height: 1, type: ['bug', 'poison']}
+]})();
 
 pokemonList.forEach(function(pokemon) {
     if (pokemon.height < 1.6) {
