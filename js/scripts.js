a7d2c5e4df660885c6f240fad49479fe4d56c1eb
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-05-27 12:13:05 +0200

    Manipulating DOM Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,52 @@
 let pokemonRepository = (function () {
 
     let pokemonList = [
-        {name: 'Pikachu', height: 0.4, type: ['electric']},
-        {name: 'Pidgeot', height: 1.5, type: ['normal', 'flying']},
-        {name: 'Horsea', height: 0.4, type: ['water']},
-        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-        {name: 'Beedrill', height: 1, type: ['bug', 'poison']}
-    ]
+        {name: 'Pikachu', height: 0.4, types: ['electric']},
+        {name: 'Pidgeot', height: 1.5, types: ['normal', 'flying']},
+        {name: 'Horsea', height: 0.4, types: ['water']},
+        {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+        {name: 'Beedrill', height: 1, types: ['bug', 'poison']}
+    ];
 
     function getAll () {
         return pokemonList;
     }
     function add (pokemon) { 
-        if (typeof pokemon.name === "string" && Object.keys(pokemonList)) {
-        pokemonList.push(pokemon)        
+        if (
+          typeof pokemon === "object" && 
+          'name' in pokemon &&
+          'height' in pokemon &&
+          'types' in pokemon
+        ) {
+          pokemonList.push(pokemon)
+        } else {
+            console.log('pokemon is not correct');
+        }        
     }
-}   
+
+    function addListItem (pokemon){
+        let container = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listItem.appendChild(button);
+        container.appendChild(listItem);
+    }
 
     return {
         getAll: getAll,
-        add: add            
-    }  
-
+        add: add, 
+        addListItem: addListItem             
+    };  
 })();
 
 // Test of return functions inside IIFE
 // console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Squirtle', height: 2});
+// pokemonRepository.add({ name: 'Squirtle', height: 2});
 // console.log(pokemonRepository.getAll());
 
 // forEach loop
 pokemonRepository.getAll().forEach(function (pokemon) {
-    if (pokemon.height < 1.6) {
-        document.write('<li>' + pokemon.name + ' ' + '(height: ' + pokemon.height + ')' + '</li>');
-    } else {
-        document.write('<li>' + pokemon.name + ' ' + '(height: ' + pokemon.height + ')' + ' - Wow, that\'s big!' + '</li>'); 
-    }   
-}
-);
+    pokemonRepository.addListItem(pokemon);      
+});
