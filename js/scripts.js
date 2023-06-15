dd8180f5daee93fbf5e7d2535518e287a4bce8e7
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-06-16 01:29:36 +0200

    Updating typesElement

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,13 @@ let pokemonRepository = (function() {
 
             let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
             
-            let typesElement = $("<p>" + "types : " + item.types + "</p>");
+            pokemonTypes = item.types;
+            for (let i = 0; i < pokemonTypes.length; i++) {
+                if (pokemonTypes.hasOwnProperty("type") === true){
+                    let typesElement = $("<p>" + "types : " + type["name"] + "</p>");
+                    return typesElement;  
+                }
+            }
 
             let abilitiesElement = $("<p>" + "abilities : " + item.abilities + "</p>"); 
 
